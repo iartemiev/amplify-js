4bff2248c58ff75d06f0ec96207264ba58435a8b
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2021-02-21 11:44:45 -0500

    include es2019.object lib

--- packages/datastore/src/util.ts
+++ packages/datastore/src/util.ts
@@ -467,7 +467,10 @@ export function objectsEqual(objA: object, objB: object): boolean {
 	let a = objA;
 	let b = objB;
 
-	if (Array.isArray(a) && !Array.isArray(b)) {
+	if (
+		(Array.isArray(a) && !Array.isArray(b)) ||
+		(Array.isArray(b) && !Array.isArray(a))
+	) {
 		return false;
 	}
 
